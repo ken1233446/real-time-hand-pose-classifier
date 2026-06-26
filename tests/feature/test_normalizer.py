1a65b1d6c0724351f24181769f398da98ce26c94
Keven C Niere <kvnnr.business@gmail.com>
2026-06-26 15:46:01 +0800

    test: add unit test/s for wrist-relative landmark translation (Normalization module)

--- tests/feature/test_normalizer.py
+++ tests/feature/test_normalizer.py
@@ -1,9 +1,11 @@
 from src.feature.normalizer import Normalizer
 import pytest
+import random as r
 
-
+#Test for validation feature
 class TestNormalizerValidation():
     VALID_LANDMARKS = 21
+
     #normalizer | self.valid_input
     def setup_method(self):
         """
@@ -46,4 +48,70 @@ class TestNormalizerValidation():
     #Test if the Landmark is valid
     def test_validate_valid_input(self):
         self.normalizer.validate(self.valid_landmarks)
+
+#Test translating landmarks relative to wrist
+class TestTranslateToRelativeToWrist():
+
+    WRIST_LANDMARK_START = 0
+    WRIST_LANDMARK_END = 3
+    TOTAL_COORDINATES = 63
+
+    #self.translate | self.valid_landmark
+    def setup_method(self):
+        """
+        This runs automatically before every individual test.
+        It ensures a fresh instance of Normalizer is always available.
+        """
+        self.translate = Normalizer() #Initialize the self.translate_relative
+        #Produces random valid float for valid landmark
+        self.valid_landmark = [(float(i), float(i + 1), float(i + 2)) for i in range(21)]
     
+    #Test if the relative landmarks data type is a list
+    def test_relative_landmark_data_type(self):
+        
+        #Store if the raw landmark into sample_coordinates list
+        sample_coordinates = self.translate.trans_relative_wrist(self.valid_landmark)
+        #Check if the relative landmark is a list
+        assert isinstance(sample_coordinates, list), \
+        "Relative Coordinates is not a list."
+
+    #Test if the relative landmarks is empty
+    def test_relative_landmark_is_empty(self):
+        
+        sample_landmark = self.translate.trans_relative_wrist(self.valid_landmark)
+        #Check if it's empty
+        assert len(sample_landmark) > 0,\
+        "The Relative landmarks must not be empty"
+
+    #Test if the wrist is the NEW origin
+    def test_wrist_new_origin(self):
+
+        #Store in the relative_to_wrist the translated landmark from self.valid_landmark
+        relative_to_wrist = self.translate.trans_relative_wrist(self.valid_landmark)
+
+        #Check if the wrist Landmark (0-3) are all ZERO (new origin)
+        assert all(i == 0 for i in relative_to_wrist[self.WRIST_LANDMARK_START:self.WRIST_LANDMARK_END]), \
+        "Error: Wrist landmark base values are not properly zeroed out."
+
+    #Test if all landmark are relative to wrist
+    def test_landmark_relative_to_wrist(self):
+
+        #Sample data !(ASSUME VALID RAW LANDMARKS)!
+        sample_landmark = [ (5.0, 10.0, 20.0),   
+                             (7.0, 15.0, 22.0),
+                             (3.0, 12.0, 18.0),]
+        sample_expected_landmark =  [0.0, 0.0, 0.0, 2.0, 5.0, 2.0, -2.0, 2.0, -2.0]
+                                    
+        #Store the sample relative coordinates
+        sample_relative_coord = self.translate.trans_relative_wrist(sample_landmark)
+        #Checks if the landmarks are properly subtract by WRIST (ORIGIN)
+        assert sample_relative_coord == sample_expected_landmark, \
+            "Relative coordinates do not match the expected values."
+
+    #Test if the total coordinates are 63    
+    def test_landmark_relative_have_63_coord(self):
+
+        sample_landmark = self.translate.trans_relative_wrist(self.valid_landmark)
+        #Check if the relative landmarks are 63 coordinates
+        assert len(sample_landmark) == self.TOTAL_COORDINATES,\
+        "The total coordinates must be 63"
---
+++ try.py
@@ -0,0 +1,6 @@
+ken =[0,2,0,2,6,123,151,6]
+
+if all(i == 0 for i in ken[0:3]):
+    print('You are right')
+else:
+    print("fuck you nigga")
